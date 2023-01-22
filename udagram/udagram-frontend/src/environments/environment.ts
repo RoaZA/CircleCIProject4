e932cd6c0a15e0c48cc87e0d23539a1ef3f7c450
Roa Al Nazawi <r.nazawi@energy.gov.sa>
2023-01-23 00:51:21 +0300

    rebuild21

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-kkccfayz.us-east-1.elasticbeanstalk.com/api/v0/feed'
+  apiHost: 'http://udagram-api-dev.eba-kkccfayz.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
