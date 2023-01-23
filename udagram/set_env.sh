3f9e02d17461ae1cc2b84fae309808c9c658904e
Roa Al Nazawi <r.nazawi@energy.gov.sa>
2023-01-23 10:42:01 +0300

    rebuild21

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,9 +1,9 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=a7jjxVqb7sjvHpBHFlNf
-export POSTGRES_HOST=udagramserver.cdtxtdb1lwy0.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=udagramdb
+export POSTGRES_USERNAME=roaAdmin
+export POSTGRES_PASSWORD=RDB_4321
+export POSTGRES_HOST=db-udagram.cdtxtdb1lwy0.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::rise-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
